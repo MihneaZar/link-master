465504e2eec720142ec7dac08bb95eeda4c29b1f
Mihnea-Bogdan ZAROJANU <mihnea.zarojanu@stud.acs.upb.ro>
2026-04-28 21:57:35 +0300

    Moved shared stuff to shared.py, upload test-run in setup, master token directly

--- link_list.py
+++ link_list.py
@@ -1,163 +1,30 @@
+from readchar import key
 from setup import setup
-import sys
+from shared import *
 import os
-
-HOMEPATH = os.path.dirname(os.path.realpath(__file__))
 
 if not os.path.exists(f'{HOMEPATH}/.paths') or not os.path.isdir(f'{HOMEPATH}/json_data'):
     setup()
 
-paths = [line.replace('\n', '') for line in open(f'{HOMEPATH}/.paths').readlines()]
-CONSOLE_PATH = paths[0]
+CONSOLE_PATH = open(f'{HOMEPATH}/.paths').readline()
 
 if not os.path.exists(f'{CONSOLE_PATH}/ConsoleListInterface.py'):
     print("Path to 'ConsoleListInterface.py' is broken, runnning setup again.")
     setup()
-    paths = [line.replace('\n', '') for line in open(f'{HOMEPATH}/.paths').readlines()]
-    CONSOLE_PATH = paths[0]
+    CONSOLE_PATH = open(f'{HOMEPATH}/.paths').readline()
 
 sys.path.append(CONSOLE_PATH)
 
-from ConsoleListInterface import ConsoleListInterface, waitForEnter # pyright: ignore[reportMissingImports]
-from readchar import readkey, key
+from ConsoleListInterface import ConsoleListInterface # pyright: ignore[reportMissingImports]
 from send2trash import send2trash
-from blessed import Terminal
-from functools import reduce
 import subprocess
 import requests
-import json
-
-paths += [""] * (3 - len(paths)) # adding empty strings so that the following commands wont raise an error
-
-KEEP_EMAIL = ""
-KEEP_TOKEN = paths[1]
-KEEP_FILE  = paths[2]
-
-if not os.path.exists(KEEP_FILE):
-    KEEP_FILE = f'{HOMEPATH}/.keep_state.json'
-
-term = Terminal()
-
-JSONFOLDER = f'{HOMEPATH}/json_data/'
 
 sys.stderr = open(f'{HOMEPATH}/errors.txt', "a")
 
 # for custom input in the link, such as show season and episode
 LINK_INPUT_START = '\\{'
 LINK_INPUT_END   = '\\}'
-
-FILENAME     = "Filename"
-DATA         = "Data"
-DESCRIPTIONS = "Description List"
-DESC         = "Description"
-INCOGNITO    = "Incognito"
-LINKS        = "Links"
-
-
-def yes_or_no(question, default_answer="yes", other_options=[], newline=True):
-    options = ["yes", "no"] + list(map(lambda opt: opt.lower(), other_options))
-    list_other_options = list(filter(lambda opt: opt != "", other_options))
-    list_other_options = '/' + reduce(lambda o1, o2: o1+'/'+o2, list_other_options) if list_other_options else ""
-    if newline:
-        print(f'{question}\nY/N{list_other_options}', flush=True)
-    else: 
-        print(f'{question} (Y/N{list_other_options}): ', end='', flush=True)
-
-    answer = readkey().lower()
-    while not any(option.startswith(answer) for option in options):
-        if answer == key.ENTER:
-            if "" in options:
-                print()
-                return ""
-            else:
-                print(default_answer.capitalize())
-                return default_answer
-        
-        answer = readkey().lower()
-
-    for option in options:
-        if option.startswith(answer):
-            print(option.capitalize())
-            if newline:
-                print()
-            return option
-
-    return None
-
-
-def gkeep_upload(press_enter=True):
-    print("Starting Google Keep upload...")
-    
-    import gkeepapi
-
-    keep = gkeepapi.Keep()
-
-    print("Downloading newest versions of notes...")
-    if os.path.exists(KEEP_FILE):
-        keep.authenticate(KEEP_EMAIL, KEEP_TOKEN, state=json.load(open(KEEP_FILE)))
-    else:
-        keep.authenticate(KEEP_EMAIL, KEEP_TOKEN)
-
-    label = keep.findLabel('Link Master')
-    if not label:
-        label = keep.createLabel('Link Master')
-
-
-    print("Deleting previous notes in label 'Link Master'...")
-    for note in keep.find(labels=[keep.findLabel('Link Master')]):
-        note.delete()
-
-
-    print("Adding new notes locally...")
-
-    # warning note for the 'Link Master' label, since all notes in label get deleted on upload
-    note = keep.createNote("DO NOT ADD THIS LABEL", "All notes with this label are deleted on upload from Link Master app.")
-    note.labels.add(label)
-    for json_file in os.listdir(JSONFOLDER):
-        link_data = json.load(open(f'{JSONFOLDER}{json_file}'))
-
-        # checking if link list has data
-        if link_data[DATA]:
-            note_text = ""
-            for entry in link_data[DATA]:
-                entry_text = entry[DESC] + ":"
-
-                pos = 1
-                for link in entry[LINKS]:
-                    counting = f'{pos}.\n' if 2 <= len(entry[LINKS]) else "- " # numbered links if multiple
-                    entry_text += f'\n{counting}{link}'  
-
-                    pos += 1
-            
-                note_text += entry_text + "\n\n"
-
-            note_text = note_text[:-2] # removing trailing newlines
-
-            note = keep.createNote(json_file[:json_file.rfind('.')], note_text)
-            note.labels.add(label)
-            note.archived = True
-
-        # if link list has no data, create empty keep note
-        else:
-            note = keep.createNote(json_file[:json_file.rfind('.')])
-            note.labels.add(label)
-            note.archived = True
-
-
-    print("Uploading new notes...")
-    keep.sync()
-
-    print("Saving newest Google Keep state (for faster uploads in the future)...")
-
-    # Store Keep cache
-    with open(KEEP_FILE, 'w') as file:
-        json.dump(keep.dump(), file)
-
-    print("Upload complete.\n")
-
-    if press_enter:
-        print("Press enter to continue.")
-        waitForEnter()
 
 
 def print_entry_details(entry):
@@ -639,7 +506,7 @@ def link_list_loop(console, json_file_path, saved_pos):
             console.exitInterface()
 
             # uploading to google keep if changes to links haven't been uploaded
-            if console.upload and KEEP_TOKEN:
+            if console.upload:
                 try:
                     gkeep_upload(False)
                 except Exception as e:
@@ -852,7 +719,7 @@ def json_file_loop(console, saved_pos=0):
             console.exitInterface()
             
             # uploading to google keep if changes to links haven't been uploaded
-            if console.upload and KEEP_TOKEN:
+            if console.upload:
                 try:
                     gkeep_upload(False)
                 except Exception as e:
--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
+from shared import HOMEPATH, KEEP_EMAIL, yes_or_no, gkeep_upload
+import sys
 import os 
 
-HOMEPATH = os.path.dirname(os.path.realpath(__file__))
+sys.stderr = open(f'{HOMEPATH}/errors.txt', "a")
 
-KEEP_EMAIL = ""
+CONSOLE_PATH_ERROR = "Path to ConsoleListInterface.py is broken."
 
 def setup():
     if not os.path.exists(f'{HOMEPATH}/json_data'):
@@ -12,7 +14,7 @@ def setup():
 
     paths = []
     if os.path.exists(f'{HOMEPATH}/.paths'):
-        paths  = [line.replace('\n', '') for line in open(f'{HOMEPATH}/.paths').readlines()]
+        paths = [line.replace('\n', '') for line in open(f'{HOMEPATH}/.paths').readlines()]
         
     paths += [""] * (3 - len(paths)) # adding empty strings so that the following commands wont raise an error
 
@@ -20,14 +22,14 @@ def setup():
     KEEP_TOKEN   = paths[1]
     KEEP_FILE    = paths[2]
 
-    if not os.path.exists(f'{CONSOLE_PATH}/ConsoleListInterface.py'):
-        CONSOLE_PATH = ""
+    if CONSOLE_PATH and not CONSOLE_PATH.isspace() and not os.path.exists(f'{CONSOLE_PATH}/ConsoleListInterface.py'):
+        CONSOLE_PATH = CONSOLE_PATH_ERROR
 
-    if not os.path.exists(KEEP_FILE):
-        KEEP_FILE = ""
-
-    if CONSOLE_PATH:
+    if CONSOLE_PATH and CONSOLE_PATH != CONSOLE_PATH_ERROR:
         print(f"Current saved directory for 'ConsoleListInterface.py':\n{CONSOLE_PATH}")
+    if CONSOLE_PATH == CONSOLE_PATH_ERROR:
+        print(CONSOLE_PATH)
+        CONSOLE_PATH = ""
     print("Please type path to directory of ConsoleListInterface.py (or leave empty to ", end = '')
     if CONSOLE_PATH:
         print("keep current path): ")
@@ -58,54 +60,70 @@ def setup():
         CONSOLE_PATH = console_path
 
     if not CONSOLE_PATH:
+        print("Path to ConsoleListInterface.py not provided, setup is aborted.\n")
         exit()
 
-    try:
-        import gpsoauth
-    except:
-        print("'gpsoauth' library missing.\nInstall library in order to be able to receive the token needed for uploading link lists to Google Keep.\nHowever, Link Master can be used without this feature.")
-        return
-
     print("\nThe following is for saving Link Lists to Google Keep. \
-          \nFor this, the Gmail address and Master Token are required. \
+          \nFor this, a Master Token is required. \
           \nAdditional details are in README.md.\n")
-
+    
     if KEEP_TOKEN:
         print("There is a saved Master Token, but you can change it now.")
-    print("In order to obtain the Google Master Token, a Google Access Token is required. \
-          \nHere is how to obtain one: https://github.com/rukins/gpsoauth-java/blob/b74ebca999d0f5bd38a2eafe3c0d50be552f6385/README.md#receiving-an-authentication-token \
-          \nNote: Access Tokens expire quickly, so if this fails, rerun this setup.")
-     
-    print("Please enter Google Access Token (or leave empty to ", end = '')
-    if KEEP_TOKEN:
-        print("keep current Master Token): ")
-    else:
-        print("skip): ")
-    
-    access_token = input()
-    while True:    
-        if access_token and not access_token.isspace():
-            try:
-                master_response = gpsoauth.exchange_token(KEEP_EMAIL, access_token, '0123456789abcdef')
-                if master_response['Token']:
-                    print("Authentification is successful, Master Token obtained.")
-                    KEEP_TOKEN = master_response['Token']
-                    break
-                else:
-                    access_token = input("Authentification failed, try again, or leave this field empty to skip:\n")
-            except:
-                access_token = input("Authentification failed, try again, or leave this field empty to skip:\n")
-        else:
-            break
 
-    print()
+    direct_token = (yes_or_no("Do you already have a Master Token?", default_answer="no") == "yes")
+    # print()
+
+    if direct_token:
+        print("Please enter Google Master Token (or leave empty to ", end = '')
+        if KEEP_TOKEN:
+            print("keep current Master Token): ")
+        else:
+            print("cancel Google Keep setup): ")
+        keep_token = input()
+        
+        if keep_token and not keep_token.isspace():
+            KEEP_TOKEN = keep_token
+    
+    else:
+        try:
+            import gpsoauth
+            print("To obtain a Google Access Token, follow the instructions at: \
+                  \nhttps://github.com/rukins/gpsoauth-java/blob/b74ebca999d0f5bd38a2eafe3c0d50be552f6385/README.md#receiving-an-authentication-token. \
+                  \nThen please enter Google Access Token (or leave empty to ", end = '')
+            if KEEP_TOKEN:
+                print("keep current Master Token): ")
+            else:
+                print("cancel Google Keep setup): ")
+            
+            access_token = input()
+            while True:    
+                if access_token and not access_token.isspace():
+                    try:
+                        master_response = gpsoauth.exchange_token(KEEP_EMAIL, access_token, '0123456789abcdef')
+                        if master_response['Token']:
+                            print("Authentification is successful, Master Token obtained.")
+                            KEEP_TOKEN = master_response['Token']
+                            break
+                        else:
+                            access_token = input("Authentification failed, try again, or leave this field empty to skip:\n")
+                    except:
+                        access_token = input("Authentification failed, try again, or leave this field empty to skip:\n")
+                else:
+                    break
+        except:
+            print("'gpsoauth' library missing. \
+                  \nInstall library in order to obtain Google Master Token with an Access Token. \
+                  \nHowever, Link Master can be used without this feature.")
+
     if not KEEP_TOKEN:
-        print("Master Token not acquired.\nFinishing setup.")
-        open(f'{HOMEPATH}/.paths', 'w').write(f'{CONSOLE_PATH}\n{KEEP_EMAIL}\n{KEEP_TOKEN}\n{KEEP_FILE}\n')
+        print("Master Token missing.\nFinishing setup.\n")
+        open(f'{HOMEPATH}/.paths', 'w').write(f'{CONSOLE_PATH}\n{KEEP_TOKEN}\n{KEEP_FILE}\n')
         return
 
+    print()
+
     if KEEP_FILE:
-        print(f"Current saved Google Keep Cache File:\n{KEEP_FILE}") 
+        print(f"Current saved Google Keep cache file:\n{KEEP_FILE}") 
     print("Please enter path to cache file (or leave empty to ", end = '')
     if KEEP_FILE:
         print("keep current cache): ")
@@ -119,11 +137,13 @@ def setup():
         if keep_file[-1] == '"':
             keep_file = keep_file[:-1]
     
-    while (keep_file and not keep_file.isspace() and not os.path.exists(keep_file)) \
+    while (keep_file and not keep_file.isspace() and (not os.path.exists(keep_file) or os.path.isdir(keep_file))) \
             or ('./' in keep_file or '.\\' in keep_file): 
         # avoiding relative paths, they might cause issues
         if ('./' in keep_file or '.\\' in keep_file): 
             keep_file = input("Please use the absolute path:\n") 
+        elif os.path.isdir(keep_file):
+            keep_file = input("That is directory, not a file:\n") 
         else: 
             keep_file = input("Keep Cache not found, please try again:\n") 
         if keep_file:
@@ -135,7 +155,26 @@ def setup():
     if keep_file:
         KEEP_FILE = keep_file
 
-    open(f'{HOMEPATH}/.paths', 'w').write(f'{CONSOLE_PATH}\n{KEEP_EMAIL}\n{KEEP_TOKEN}\n{KEEP_FILE}\n')
+    open(f'{HOMEPATH}/.paths', 'w').write(f'{CONSOLE_PATH}\n{KEEP_TOKEN}\n{KEEP_FILE}\n')
+
+    print()
+    try_upload = yes_or_no("Would you like to DO a test upload (also creates the Keep Cache, which is slower on the first upload)?") == "yes"
+
+    if try_upload:
+        try:
+            gkeep_upload(press_enter=False)
+        except Exception as e:
+            print(f"\nError: '{type(e).__name__}'.")
+            if type(e).__name__ == 'LoginException':
+                print("Master Token authentification failed, check that the token is correct or retry obtaining it with an Access Token.")
+            elif type(e).__name__ == 'ModuleNotFoundError':
+                print("The 'gkeepapi' library is missing.\nPlease install it with 'pip3 install gkeepapi' to be able to upload the link lists to Google Keep.")
+            elif type(e).__name__ == 'FileNotFoundError' or type(e).__name__ == 'OSError':
+                print("The cache file path is broken.\nThis path will be removed, but you can change it back to an existing file by rerunning this setup.")
+                open(f'{HOMEPATH}/.paths', 'w').write(f'{CONSOLE_PATH}\n{KEEP_TOKEN}\n\n')
+            else:
+                print("Unknown error type.")
+            print()
 
 
 if __name__ == "__main__":
